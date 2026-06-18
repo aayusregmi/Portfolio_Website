afeafe38c08bd8779c2aab02bdf33d0d71208c96
Aayus Regmi <aayusregmi721@gmail.com>
2026-06-18 20:08:20 +0545

    working

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu-bar");
+const slide_bar = document.querySelector(".slidebar-menu");
+const myPhoto = document.querySelector(".myPhoto")
+menu.addEventListener("click" , ()=>{
+    slide_bar.classList.toggle("openSlide");
+    myPhoto.classList.toggle("removeBorder");
+})
